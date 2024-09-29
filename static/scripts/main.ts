536a522699273230d14706eb20dfee039696bc7c
kotelnikov-vladislav <vladislav.kotelnikov@internet.ru>
2024-09-29 16:05:06 +0300

    main: добавлен слайдер для команды

--- static/scripts/main.ts
+++ static/scripts/main.ts
@@ -1,11 +1,29 @@
 import 'vite/modulepreload-polyfill';
 import {AudioPlayer} from './modules/audio-player.ts';
 import {ShowCarousel} from './modules/show-carousel/index.ts';
+import EmblaCarousel, {EmblaCarouselType} from 'embla-carousel';
+import { OPTIONS, PLUGINS } from './modules/show-carousel/models/const/show-carousel.const.ts';
 
-new AudioPlayer('radio', 'radio--active').init()
+new AudioPlayer('radio', 'radio--active').init();
 new ShowCarousel({
     carouselId: 'show-carousel',
     indicatorsId: 'show-carousel-indicators',
     carouselCtrlId: 'show-carousel-ctrl',
     indicatorActiveClassName: 'show-carousel__indicator--active'
-}).init()
+}).init();
+
+
+const teamCarouselElement = document.getElementById('team-carousel');
+const teamBtnPrev = document.getElementById('team-btn-prev');
+const teamBtnNext = document.getElementById('team-btn-next');
+
+const teamCarousel = EmblaCarousel(teamCarouselElement, OPTIONS);
+
+teamBtnPrev?.addEventListener('click', () => {
+    teamCarousel.scrollPrev();
+})
+
+
+teamBtnNext?.addEventListener('click', () => {
+    teamCarousel.scrollNext();
+})

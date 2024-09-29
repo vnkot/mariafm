8188927ae5f3ee5a0d66ff51a6e00f9b17eb9afc
kotelnikov-vladislav <vladislav.kotelnikov@internet.ru>
2024-09-29 18:07:16 +0300

    main: добавлена возможность работать с аудио

--- static/scripts/main.ts
+++ static/scripts/main.ts
@@ -4,14 +4,14 @@ import {ShowCarousel} from './modules/show-carousel/index.ts';
 import {TeamCarousel} from './modules/team-carousel/index.ts';
 
 new AudioPlayer('radio', 'radio--active').init();
-new ShowCarousel({
-    carouselId: 'show-carousel',
-    indicatorsId: 'show-carousel-indicators',
-    carouselCtrlId: 'show-carousel-ctrl',
-    indicatorActiveClassName: 'show-carousel__indicator--active'
-}).init();
-new TeamCarousel({
-    btnNextId: 'team-btn-next',
-    btnPrevId: 'team-btn-prev',
-    carouselId: 'team-carousel'
-}).init()
+// new ShowCarousel({
+//     carouselId: 'show-carousel',
+//     indicatorsId: 'show-carousel-indicators',
+//     carouselCtrlId: 'show-carousel-ctrl',
+//     indicatorActiveClassName: 'show-carousel__indicator--active'
+// }).init();
+// new TeamCarousel({
+//     btnNextId: 'team-btn-next',
+//     btnPrevId: 'team-btn-prev',
+//     carouselId: 'team-carousel'
+// }).init()
--- static/scripts/modules/audio-player.ts
+++ static/scripts/modules/audio-player.ts
@@ -1,37 +1,51 @@
 export class AudioPlayer {
-    private readonly elementId: string;
+    private readonly playerId: string;
     private readonly classNameActive: string;
+    
+    private playerElement!: HTMLElement;
+    private audioElement!: HTMLAudioElement;
+
     private audioPlayerState: boolean = false;
+
 
     /**
      * Представляет аудио-плеер с возможностью активации.
-     * @param {string} elementId - идентификатор HTML-элемента, представляющего аудио-плеер.
+     * @param {string} playerId - идентификатор HTML-элемента, представляющего аудио-плеер.
      * @param {string} classNameActive - класс-модификатор, добавляющийся при активации аудио-плеера.
      */
-    constructor(elementId: string, classNameActive: string) {
-        this.elementId = elementId;
+    constructor(playerId: string, classNameActive: string) {
+        this.playerId = playerId;
         this.classNameActive = classNameActive;
+    
+        this.getElements();
+    }
+    
+    private getElements() {
+        const element = document.getElementById(this.playerId);
+        const audio = element?.getElementsByTagName('audio')[0];
+        
+        if (!audio) throw new Error(`Не удалось найти audio-элемент в ${element}`);
+        if (!element) throw new Error(`Не удалось найти html-элемент по переданному идентификатору ${this.playerId}`);
+
+        this.audioElement = audio;
+        this.playerElement = element;
     }
 
-    private eventListener(element: HTMLElement) {
-        element.addEventListener('click', () => {
-            this.audioPlayerState = !this.audioPlayerState; // Используем состояние класса
+    private eventListener() {
+        this.playerElement.addEventListener('click', () => {
+            this.audioPlayerState = !this.audioPlayerState;
 
             if (this.audioPlayerState) {
-                element.classList.add(this.classNameActive);
+                this.playerElement.classList.add(this.classNameActive);
+                this.audioElement.play();
             } else {
-                element.classList.remove(this.classNameActive);
+                this.playerElement.classList.remove(this.classNameActive);
+                this.audioElement.pause();
             }
         });
     }
 
-    public init() {
-        const element = document.getElementById(this.elementId);
-
-        if (!element) {
-            throw new Error(`Не удалось найти html-элемент по переданному идентификатору ${this.elementId}`);
-        }
-
-        this.eventListener(element);
+    init() {
+        this.eventListener();
     }
 }

e0aaab2a151e48bd4528f0efe48952491c962b61
kotelnikov-vladislav <vladislav.kotelnikov@internet.ru>
2024-10-02 20:06:12 +0300

    main: чутка поправлена верстка

--- static/src/scripts/main.ts
+++ static/src/scripts/main.ts
@@ -4,19 +4,19 @@ import {ShowCarousel} from './modules/show-carousel/show-carousel.ts';
 import {TeamCarousel} from './modules/team-carousel/team-carousel.ts';
 import {InteractiveMap} from './modules/interactive-map.ts';
 
-new AudioPlayer('radio', 'radio--active').init();
-new ShowCarousel({
-    carouselId: 'show-carousel',
-    indicatorsId: 'show-carousel-indicators',
-    carouselCtrlId: 'show-carousel-ctrl',
-    indicatorActiveClassName: 'show-carousel__indicator--active'
-}).init();
-new TeamCarousel({
-    btnNextId: 'team-btn-next',
-    btnPrevId: 'team-btn-prev',
-    carouselId: 'team-carousel'
-}).init()
-new InteractiveMap('footer-map', [58.583945, 49.659408]).init();
+// new AudioPlayer('radio', 'radio--active').init();
+// new ShowCarousel({
+//     carouselId: 'show-carousel',
+//     indicatorsId: 'show-carousel-indicators',
+//     carouselCtrlId: 'show-carousel-ctrl',
+//     indicatorActiveClassName: 'show-carousel__indicator--active'
+// }).init();
+// new TeamCarousel({
+//     btnNextId: 'team-btn-next',
+//     btnPrevId: 'team-btn-prev',
+//     carouselId: 'team-carousel'
+// }).init()
+// new InteractiveMap('footer-map', [58.583945, 49.659408]).init();
 
 // TODO: временная история
 const school = document.getElementById('school')!;

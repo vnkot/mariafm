1d72007a4436ba90f27345d4b8051e32deced6fb
kotelnikov-vladislav <vladislav.kotelnikov@internet.ru>
2024-10-30 20:53:45 +0300

    main: поправлено поведение элементов карусели команды

--- static/src/scripts/modules/team-carousel/model/const/team-carousel.const.ts
+++ static/src/scripts/modules/team-carousel/model/const/team-carousel.const.ts
@@ -2,7 +2,6 @@
 import { EmblaOptionsType } from 'embla-carousel/components/Options';
 
 export const OPTIONS: EmblaOptionsType = {
-	loop: true,
 	startIndex: 0,
 	slidesToScroll: 'auto',
 };
--- static/src/scripts/modules/team-carousel/team-carousel.ts
+++ static/src/scripts/modules/team-carousel/team-carousel.ts
@@ -1,64 +1,67 @@
-import EmblaCarousel, {EmblaCarouselType} from 'embla-carousel';
-import {OPTIONS} from './model/const/index.ts';
+import EmblaCarousel, { EmblaCarouselType } from 'embla-carousel';
+import { OPTIONS } from './model/const/index.ts';
 
 interface ITeamCarouselConfig {
-    btnNextId: string;
-    btnPrevId: string;
-    carouselId: string;
+	btnNextId: string;
+	btnPrevId: string;
+	carouselId: string;
 }
 
 export class TeamCarousel {
-    private readonly config: ITeamCarouselConfig;
+	private readonly config: ITeamCarouselConfig;
 
-    private carouselElement!: HTMLElement;
-    private btnPrevElement!: HTMLButtonElement;
-    private btnNextElement!: HTMLButtonElement;
+	private carouselElement!: HTMLElement;
+	private btnPrevElement!: HTMLButtonElement;
+	private btnNextElement!: HTMLButtonElement;
 
-    private carousel!: EmblaCarouselType;
+	private carousel!: EmblaCarouselType;
 
-    constructor(config: ITeamCarouselConfig) {
-        this.config = config;
+	constructor(config: ITeamCarouselConfig) {
+		this.config = config;
 
-        this.getElements();
-        this.createCarousel();
-    }
+		this.getElements();
+		this.createCarousel();
+	}
 
-    private getElements() {
-        const btnPrevElement = document.getElementById(this.config.btnPrevId)
-        const btnNextElement = document.getElementById(this.config.btnNextId);
-        const carouselElement = document.getElementById(this.config.carouselId);
+	private getElements() {
+		const btnPrevElement = document.getElementById(this.config.btnPrevId);
+		const btnNextElement = document.getElementById(this.config.btnNextId);
+		const carouselElement = document.getElementById(this.config.carouselId);
 
-        if (!btnPrevElement || !btnNextElement || !carouselElement) {
-            throw new Error('Не удалось получить необходимые элементы для карусели')
-        }
+		if (!btnPrevElement || !btnNextElement || !carouselElement) {
+			throw new Error(
+				'Не удалось получить необходимые элементы для карусели'
+			);
+		}
 
-        this.carouselElement = carouselElement;
-        this.btnPrevElement = btnPrevElement as HTMLButtonElement;
-        this.btnNextElement = btnNextElement as HTMLButtonElement;
-    }
+		this.carouselElement = carouselElement;
+		this.btnPrevElement = btnPrevElement as HTMLButtonElement;
+		this.btnNextElement = btnNextElement as HTMLButtonElement;
+	}
 
-    private createCarousel() {
-        this.carousel = EmblaCarousel(this.carouselElement, OPTIONS);
-    }
+	private createCarousel() {
+		this.carousel = EmblaCarousel(this.carouselElement, OPTIONS);
+	}
 
-    private checkScroll() {
-        const isScrollPrev = this.carousel.canScrollPrev();
-        const isScrollNext = this.carousel.canScrollNext();
+	private addEventListenerForCtrl() {
+		this.btnPrevElement.addEventListener('click', () => {
+			if (!this.carousel.canScrollPrev()) {
+				return this.carousel.scrollTo(
+					this.carousel.slideNodes().length - 1
+				);
+			}
 
-        this.btnPrevElement.disabled = !isScrollPrev;
-        this.btnNextElement.disabled = !isScrollNext;        
-    }
+			this.carousel.scrollPrev();
+		});
+		this.btnNextElement.addEventListener('click', () => {
+			if (!this.carousel.canScrollNext()) {
+				return this.carousel.scrollTo(0);
+			}
+			this.carousel.scrollNext();
+		});
+	}
 
-    private addEventListenerForCtrl() {
-        this.btnPrevElement.addEventListener('click', () => this.carousel.scrollPrev());
-        this.btnNextElement.addEventListener('click', () => this.carousel.scrollNext());
-
-        this.carousel.on('init', () => this.checkScroll());
-        this.carousel.on('scroll', () => this.checkScroll());
-        this.carousel.on('resize', () => this.checkScroll());
-    }
-
-    init() {
-        this.addEventListenerForCtrl();
-    }
+	init() {
+		this.addEventListenerForCtrl();
+	}
 }
